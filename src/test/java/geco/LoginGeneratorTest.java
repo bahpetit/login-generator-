56168a1930489426a5bf237447729388738cde9d
bahpetit <abdoulayediouldebah@gmail.com>
2016-11-22 17:05:28 +0100

    fix #3

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -16,14 +16,26 @@ public class LoginGeneratorTest {
         LoginGenerator loginGenerator = new LoginGenerator(loginService);
         assertTrue("Le login de Paul Durand est: PDUR", "PDUR".equals(loginGenerator.generateLoginForNomAndPrenom("Durand", "Paul")));
     }
+
     @Test
     public void generateLoginForNomAndPrenom_Not_JRAL2() throws Exception {
         //Given
         loginService = new LoginService(new String[] {"JROL", "BPER","CGUR", "JDUP", "JRAL", "JRAL1"});
         LoginGenerator loginGenerator = new LoginGenerator(loginService);
-        assertFalse("Le login de John Ralling n'est pas: JRAL2", !("JRAL2".equals(loginGenerator.generateLoginForNomAndPrenom("Ralling", "John"))));
+        assertTrue("Le login de John Ralling n'est pas: JRAL2", ("JRAL2".equals(loginGenerator.generateLoginForNomAndPrenom("Ralling", "John"))));
     }
-
-
-
+    @Test
+    public void generateLoginForNomAndPrenom_JROL1() throws Exception {
+        //Given
+        loginService = new LoginService(new String[] {"JROL", "BPER","CGUR", "JDUP", "JRAL", "JRAL1"});
+        LoginGenerator loginGenerator = new LoginGenerator(loginService);
+        assertTrue("Le login de Paul Durand est: JROL1", "JROL1".equals(loginGenerator.generateLoginForNomAndPrenom("Rolling", "Jean")));
+    }
+    @Test
+    public void generateLoginForNomAndPrenom_PDUR_accent() throws Exception {
+        //Given
+        loginService = new LoginService(new String[] {"JROL", "BPER","CGUR", "JDUP", "JRAL", "JRAL1"});
+        LoginGenerator loginGenerator = new LoginGenerator(loginService);
+        assertTrue("Le login de Paul Durand est: PDUR", "PDUR".equals(loginGenerator.generateLoginForNomAndPrenom("Dùrand", "Paul")));
+    }
 }
